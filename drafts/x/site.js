bd466f6d2f4592fd0c3dca6cf733dfc8e4cb2859
theodore-q <theo.q.er@gmail.com>
2014-06-24 15:09:08 +0100

    yess

--- drafts/x/site.js
+++ drafts/x/site.js
@@ -13,3 +13,35 @@
       });
 
     }});
+
+      $.ajax({
+
+    url: "http://content.guardianapis.com/search?section=travel",
+
+    dataType: "jsonp",
+
+    success: function(data) {
+
+      $.each(data.response.results, function () {
+
+        $( "#travel" ).append("<li>" + this['webTitle'] + "</li>");
+
+      });
+
+    }});
+
+      $.ajax({
+
+    url: "http://content.guardianapis.com/search?section=football",
+
+    dataType: "jsonp",
+
+    success: function(data) {
+
+      $.each(data.response.results, function () {
+
+        $( "#football" ).append("<li>" + this['webTitle'] + "</li>");
+
+      });
+
+    }});
