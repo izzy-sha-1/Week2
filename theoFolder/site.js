3b37d835d5854657708c6c87fadb5922868ae763
theodore-q <theo.q.er@gmail.com>
2014-06-24 18:39:14 +0100

    test

--- theoFolder/site.js
+++ theoFolder/site.js
@@ -8,40 +8,11 @@
 
       $.each(data.response.results, function () {
 
-        $( "#news" ).append("<li>" + this['webTitle'] + "</li>");
-
+        $( "#news" ).append('<a href="' + this['webUrl'] + '"><h3>' + this['webTitle'] + '<h3></a>');
+$( "#news" ).append('<ul>"' + this['fields'].trailText + '</ul>');
       });
 
     }});
 
-    $.ajax({
 
-    url: "http://content.guardianapis.com/search?section=travel",
-
-    dataType: "jsonp",
-
-    success: function(data) {
-
-      $.each(data.response.results, function () {
-
-        $( "#travel" ).append("<li>" + this['webTitle'] + "</li>");
-
-      });
-
-    }});
-
-      $.ajax({
-
-    url: "http://content.guardianapis.com/search?section=football",
-
-    dataType: "jsonp",
-
-    success: function(data) {
-
-      $.each(data.response.results, function () {
-
-        $( "#football" ).append("<li>" + this['webTitle'] + "</li>");
-
-      });
-
-    }});
+  
