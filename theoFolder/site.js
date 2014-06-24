f811a723d7d238fca0018aaec09b845bc419b029
theodore-q <theo.q.er@gmail.com>
2014-06-24 13:33:16 +0100

    yo

--- theoFolder/site.js
+++ theoFolder/site.js
@@ -1,1 +1,15 @@
-var getNews = function()
+  $.ajax({
+
+    url: "http://content.guardianapis.com/search?section=news",
+
+    dataType: "jsonp",
+
+    success: function(data) {
+
+      $.each(data.response.results, function () {
+
+        $( "#news" ).append("<li>" + this['webTitle'] + "</li>");
+
+      });
+
+    }});
