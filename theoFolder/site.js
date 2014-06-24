a2ed89c0c5d451e4479199658cd3d6dbe85799f2
theodore-q <theo.q.er@gmail.com>
2014-06-24 15:04:29 +0100

    test

--- theoFolder/site.js
+++ theoFolder/site.js
@@ -13,3 +13,35 @@
       });
 
     }});
+
+    $.ajax({
+
+    url: "http://content.guardianapis.com/search?section=news",
+
+    dataType: "jsonp",
+
+    success: function(data) {
+
+      $.each(data.response.results, function () {
+
+        $( "#Travel" ).append("<li>" + this['webTitle'] + "</li>");
+
+      });
+
+    }});
+
+      $.ajax({
+
+    url: "http://content.guardianapis.com/search?section=news",
+
+    dataType: "jsonp",
+
+    success: function(data) {
+
+      $.each(data.response.results, function () {
+
+        $( "#Football" ).append("<li>" + this['webTitle'] + "</li>");
+
+      });
+
+    }});
