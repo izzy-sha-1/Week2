4085725e2a759d677094327e4be81f3f6b097622
theodore-q <theo.q.er@gmail.com>
2014-06-24 13:21:55 +0100

    Update site.js

--- theo folder/site.js
+++ theo folder/site.js
@@ -1,1 +1,7 @@
-var getNews = function()
+$.ajax({
+        url: "http://content.guardianapis.com/search?section=football",
+        dataType: "jsonp",
+        success: function( data ) {
+            $( "#weather1" ).append("<li>" + data.response.results[3].webTitle + "</li>");
+        }
+    });
