61b09ef4c333dd0c3dfba98c129a10889ae58e54
theodore-q <theo.q.er@gmail.com>
2014-06-25 12:11:40 +0100

    hey

--- theoFolder/site.js
+++ theoFolder/site.js
@@ -1,18 +1,19 @@
   var topics = ["travel", "news", "football"];
   for (i=0;i < topics.length;i++){
 
-  $.ajax({
+    $.ajax({
 
-    url: "http://content.guardianapis.com/search?q=" + topics[i] + "&show-fields=trailText",
+      url: "http://content.guardianapis.com/search?q=travel&show-fields=trailText",
 
-    dataType: "jsonp",
+      dataType: "jsonp",
 
-    success: function(data) {
-  $.each(data.response.results, function () {
+      success: function(data) {
+        $.each(data.response.results, function () {
 
-        $( "#" + topics[i] ).append('<a href="' + this['webUrl'] + '"><h3>' + this['webTitle'] + '<h3></a>');
-        $( "#" + topics[i] ).append('<ul>"' + this['fields'].trailText + '</ul>');
-      });
+          $( "#travel" ).append('<a href="' + this['webUrl'] + '"><h3>' + this['webTitle'] + '<h3></a>');
+          $( "#travel" ).append('<ul>"' + this['fields'].trailText + '</ul>');
+        });
 
-    }});
-};
+      }});
+  };
+  
