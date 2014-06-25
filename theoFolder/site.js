439c66cc9560d35e4ca7f1049359b4cba7ff78bc
theodore-q <theo.q.er@gmail.com>
2014-06-25 17:16:10 +0100

    hey

--- theoFolder/site.js
+++ theoFolder/site.js
@@ -8,13 +8,13 @@
 
     $.ajax({
 
-      url: "http://content.guardianapis.com/search?q="+ val +"&show-fields=trailText",
+      url: "http://content.guardianapis.com/search?q="+ val +"&show-fields=trailText%2CshortUrl",
 
       dataType: "jsonp",
 
       success: function(data) {
         $.each(data.response.results, function () {
-                 $( "#" + val ).append('<li><a target="_blank" href="' + this['webUrl'] + '">' + this['webTitle'] +'</a><p>' + this['fields'].trailText + '</p></li>');
+                 $( "#" + val ).append('<li><a target="_blank" href="' + this['fields'].shortUrl + '">' + this['webTitle'] +'</a><p>' + this['fields'].trailText + '</p></li>');
         });
 
       }});
