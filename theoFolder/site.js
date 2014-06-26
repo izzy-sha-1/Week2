74ba5073333570145527664b4a288e6f4c033729
theodore-q <theo.q.er@gmail.com>
2014-06-26 15:19:59 +0100

    ok

--- theoFolder/site.js
+++ theoFolder/site.js
@@ -1,3 +1,11 @@
+  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
+  <script type="text/javascript">
+  google.load("feeds", "1");
+</script>
+
+
+var bbcfeed = new google.feeds.Feed("http://feeds.bbci.co.uk/news/rss.xml");
   var topics = {'1':"travel",'2':"news",'3':"football"};
   var googleAPI = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=";
 
@@ -22,12 +30,12 @@
   
    $.ajax({
 
-      url: googleAPI + "http://feeds.bbci.co.uk/news/rss.xml" + "&callback=?",
+      url: bbcfeed,
 
       dataType: "json",
 
       success: function(data) {
-        $.each(data.responseData.entries, function () {
+        $.each(data.feed.entries, function () {
                  $( "#bbc" ).append('<li><a target="_blank" href="' + this['link'] + '">' + this['title'] +'</a><p>' + this['contentSnippet'] + '</p></li>');
         });
         console.log(data)
