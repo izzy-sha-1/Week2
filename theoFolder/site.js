b85f8f973e66ddd03ba5696b9a828d72fef906ae
theodore-q <theo.q.er@gmail.com>
2014-06-26 11:50:22 +0100

    hello

--- theoFolder/site.js
+++ theoFolder/site.js
@@ -20,3 +20,19 @@
       }});
   });
   
+
+
+$.get(http:feeds.bbci.co.uk/news/rss.xml, 
+
+  success: function (data) {
+    
+    $(data).find("entry").each(function () { // or "item" or whatever suits your feed
+        var el = $(this);
+
+        console.log("------------------------");
+        console.log("title      : " + el.find("title").text());
+        console.log("author     : " + el.find("author").text());
+        console.log("description: " + el.find("description").text());
+    });
+});
+
