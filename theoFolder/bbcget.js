b1c78583f9a2d87bda26ff9851a9bbbc164377dd
theodore-q <theo.q.er@gmail.com>
2014-06-27 15:05:43 +0100

    ok

--- theoFolder/bbcget.js
+++ theoFolder/bbcget.js
@@ -2,9 +2,10 @@ $.ajax({
       url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.digg.com/rss/index.xml",
 
       dataType: "jsonp",
-
+console.log("hello");
       fsuccess: function(data) {
         console.log(data);
+        console.log("hello world");
         $.each(data.responseData.feed.entries, function () {
                  $( "#bbc" ).append('<li><a target="_blank" href="' + this['link'] + '">' + this['title'] +'</a><p>' + this['contentSnippet'] + '</p></li>');
         });
