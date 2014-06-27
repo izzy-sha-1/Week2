cb276a778b8d882fc777069ef85487e67c353b73
theodore-q <theo.q.er@gmail.com>
2014-06-27 14:35:58 +0100

    ok

--- theoFolder/site.js
+++ theoFolder/site.js
@@ -20,3 +20,15 @@
   });
   
 
+$.ajax({
+      url https:"//ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.digg.com/rss/index.xml"
+
+      dataType: "jsonp",
+
+      fsuccess: function(data) {
+        $.each(data.responseData.feed.entries, function () {
+                 $( "#bbc" ).append('<li><a target="_blank" href="' + this['link'] + '">' + this['title'] +'</a><p>' + this['contentSnippet'] + '</p></li>');
+        });
+        console.log(data)
+
+      }});
