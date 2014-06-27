9c82fea513b77aa17b46739286911d9c63d43328
theodore-q <theo.q.er@gmail.com>
2014-06-27 15:24:13 +0100

    ok

--- theoFolder/bbcget.js
+++ theoFolder/bbcget.js
@@ -6,10 +6,10 @@
       var feed = new google.feeds.Feed("http://feeds.bbci.co.uk/news/rss.xml?edition=int");
       feed.setNumEntries(10);
 
-      success: function(feed) {
+      success: function(data) {
         console.log(data);
         console.log("hello world");
-        $.each(feed.responseData.feed.entries, function () {
+        $.each(data.responseData.feed.entries, function () {
                  $( "#bbc" ).append('<li><a target="_blank" href="' + this['link'] + '">' + this['title'] +'</a><p>' + this['contentSnippet'] + '</p></li>');
         });
         
