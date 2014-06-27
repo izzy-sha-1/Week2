1e0dec46aff91bb7e1c44d05cfa2040902751603
theodore-q <theo.q.er@gmail.com>
2014-06-27 17:36:16 +0100

    ok

---
+++ theoFolder/googlefeedtest.js
@@ -0,0 +1,33 @@
+
+
+google.load("feeds", "1");
+
+
+
+    function initialize() {
+      var feed = new google.feeds.Feed("http://feeds.bbci.co.uk/news/rss.xml?edition=int");
+      feed.setNumEntries(10);
+      console.log(feed),
+      feed.load(function(result) {
+        if (!result.error) {
+          var container = document.getElementById("feed");
+          for (var i = 0; i < result.feed.entries.length; i+=1) {
+            var entry = result.feed.entries[i];
+            var line = /*'<a href="' + entry.link + '">' +*/ entry.title /*+ '</a>' + '<p>' + entry.contentSnippet + '</p>'*/ ;
+          
+            var div = document.createElement("div");
+           
+            div.appendChild(document.createTextNode(line));
+
+
+
+            
+           // var div = document.createElement("li");
+           // div.appendChild(document.createTextNode(entry.contentSnippet));
+            
+            container.appendChild(div);
+          }
+        }
+      });
+    }
+    google.setOnLoadCallback(initialize);
