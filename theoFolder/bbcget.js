74924e59bdbe578e5922b5c8bd40db8bf4645d60
theodore-q <theo.q.er@gmail.com>
2014-06-27 14:48:12 +0100

    ok

--- theoFolder/bbcget.js
+++ theoFolder/bbcget.js
@@ -1,7 +1,7 @@
 $.ajax({
-      url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.digg.com/rss/index.xml"
+      url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.digg.com/rss/index.xml",
 
-      dataType: "json",
+      dataType: "jsonp",
 
       fsuccess: function(data) {
         $.each(data.responseData.feed.entries, function () {
